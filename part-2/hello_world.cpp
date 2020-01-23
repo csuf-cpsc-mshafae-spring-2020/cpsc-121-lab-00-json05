734aecc7c5890c8a06d60daec3bb4bc0475fa8dc
json05 <json05@cs104u-1b.acad.fullerton.edu>
2020-01-23 15:47:40 -0800

    started

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,12 +1,13 @@
 // Jason Le
 // CPSC 121-03
 // json05@csu.fullerton.edu
+// THISISREALLYCOOL
 
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[1]){
-  cout << "Hello worlds!" << endl;
+int main(int argc, char *argv[]){
+  cout << "Hello" << argv[1] << '\n';
 
   return 0;
 }
