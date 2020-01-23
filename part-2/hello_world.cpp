5c67c38e55910eb29f064651692daf39a1e782d7
json05 <json05@cs104u-1b.acad.fullerton.edu>
2020-01-23 15:25:31 -0800

    added quote

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,11 +1,12 @@
-// Oscar Mayer
-// CPSC 121-99
-// oscar.mayer@csu.fullerton.edu
+// Jason Le
+// CPSC 121-03
+// json05@csu.fullerton.edu
 
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]){
-  cout << "Hello world!" << endl;
+int main(int argc, char *argv[1]){
+  cout << "Hello worlds!" << endl;
+
   return 0;
 }
